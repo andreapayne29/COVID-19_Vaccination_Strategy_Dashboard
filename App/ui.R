f32e7e968f117e8130041acc2b9b253a55a4b415
Andie Payne <andiepayne@outlook.com>
2021-02-04 17:40:19 -0500

    finished sidebar graphics added tabs

--- App/ui.R
+++ App/ui.R
@@ -20,11 +20,24 @@ shinyUI(fluidPage(
         sidebarPanel(
             titlePanel("Strategy Settings"),
             radioButtons("minimizationChoice", "Minimize", c("Deaths", "Cases")),
+            
             h3("Initial Active Cases"),
             numericInput("originalVirusInfections", "Original Strain", 0),
             numericInput("UKVirusInfections", "UK Strain", 0),
             numericInput("southAfricaVirusInfections", "South African Strain", 0),
             numericInput("BrazilVirusInfections", "Brazilian Strain", 0),
+            
+            h3("Vaccines Administered per Unit Time"), #find out if you want per week or daily
+            numericInput("PfizerVac", "Pfizer", 0),
+            numericInput("ModernaVac", "Moderna", 0), 
+            #add a conditional warning if specified vaccine doses will not cover the gen pop by Sept 30, 2021
+            
+            h3("Strategy"),
+            selectInput("strat", "Priority Vaccination", 
+                        c("Older Generations", "Frontline Workers", "Marginalized Populations", 
+                          "Highest Population Density", "Random")),
+            radioButtons("doses", "Number of Doses", c("1", "2")),
+            numericInput("alreadyVac", "Number of People Already Vaccinated", 0),
             
             sliderInput("bins",
                         "Number of bins:",
@@ -35,7 +48,11 @@ shinyUI(fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot")
+            tabsetPanel(tabPanel("Tab 1", plotOutput("distPlot")))
+                        #tabPanel("Tab 2", plotOutput("distPlot")),
+                        #tabPanel("Tab 3", plotOutput("distPlot")),
+                        #tabPanel("Tab 4", plotOutput("distPlot")),
+                        #tabPanel("Tab 5", plotOutput("distPlot"))),
         )
     )
 ))
