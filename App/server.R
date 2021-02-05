230c09ab6c2b46a56c5145f13400479f4676a4be
Andie Payne <andiepayne@outlook.com>
2021-02-05 10:02:42 -0500

    part 1 complete

--- App/server.R
+++ App/server.R
@@ -12,15 +12,27 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-    })
-
+    output$casesText <- renderUI({HTML(paste("Here will be a time series plot of new cases 
+                                   from vaccination start to Sept 30, 2021. There 
+                                   will also be a cumulative cases plot and some 
+                                   text detailing the number of new cases from beginning to Sept 30, 2021.",
+                   "Sidebar Note: Unsure if there is enough info about the variation strains to include them",sep ="<br/>"))})
+    output$deathText <- renderText("This tab is the exact same as the Projected Cases tab except with deaths.")
+    output$vaccText <- renderText("Another time series plot along with a table of fully and partially vaccinated 
+                                  individuals. One plot details 2 dose individuals and the partial is 1 dose individuals.")
+    output$mapsText <- renderText("Map visualizations")
+    output$dataLimText <- renderUI({HTML(paste("Any and all data manipulation, limitations, and assumptions are here. Data credit will also be provided.",
+                                     "Current Data Credit: variation names from CNN https://www.cnn.com/2021/02/02/health/variant-mutation-e484k/index.html.",
+                                     "Current known limitations:",
+                                     "0-15 yr olds cannot be vaccinated by either vaccine 
+                                     and 16-17 yr olds can only be vaccinated with Pfizer. Census population data groups 15 
+                                     yr olds with 16-19 yr olds and that data must be divided appropriately.
+                                     Governmnet organizations never specify who the 'general population' is, 
+                                     current solution: subtract healthcare workforce from total adult population.", sep = "<br/>"))})
+    output$authorText <- renderText("Little blurb about me with photo and links to linkedin and github")
+    
+    
+    
+    
+    
 })
--- App/ui.R
+++ App/ui.R
@@ -13,7 +13,7 @@ library(shiny)
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("COVID-19 Canadian Vaccination Strategy"),
+    titlePanel("COVID-19 Ontario Vaccination Strategy"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
@@ -22,10 +22,10 @@ shinyUI(fluidPage(
             radioButtons("minimizationChoice", "Minimize", c("Deaths", "Cases")),
             
             h3("Initial Active Cases"),
-            numericInput("originalVirusInfections", "Original Strain", 0),
-            numericInput("UKVirusInfections", "UK Strain", 0),
-            numericInput("southAfricaVirusInfections", "South African Strain", 0),
-            numericInput("BrazilVirusInfections", "Brazilian Strain", 0),
+            numericInput("originalVirusInfections", "SARS-CoV-2 (Original Strain)", 0),
+            #unsure if i want to keep the variations
+            numericInput("UKVirusInfections", "SARS-CoV-2 variant B.1.1.7 (UK Strain)", 0),
+            numericInput("southAfricaVirusInfections", "SARS-CoV-2 variant B.1.351 (South African Strain)", 0),
             
             h3("Vaccines Administered per Unit Time"), #find out if you want per week or daily
             numericInput("PfizerVac", "Pfizer", 0),
@@ -39,20 +39,16 @@ shinyUI(fluidPage(
             radioButtons("doses", "Number of Doses", c("1", "2")),
             numericInput("alreadyVac", "Number of People Already Vaccinated", 0),
             
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-            tabsetPanel(tabPanel("Tab 1", plotOutput("distPlot")))
-                        #tabPanel("Tab 2", plotOutput("distPlot")),
-                        #tabPanel("Tab 3", plotOutput("distPlot")),
-                        #tabPanel("Tab 4", plotOutput("distPlot")),
-                        #tabPanel("Tab 5", plotOutput("distPlot"))),
+            tabsetPanel(tabPanel("Projected New Cases", htmlOutput("casesText")),
+                        tabPanel("Projected Deaths", textOutput("deathText")),
+                        tabPanel("Vaccinated Individuals", textOutput("vaccText")),
+                        tabPanel("Visualization", textOutput("mapsText")),
+                        tabPanel("Data Manipulation and Limitations", htmlOutput("dataLimText")), 
+                        tabPanel("Author", textOutput("authorText")))
         )
     )
 ))
