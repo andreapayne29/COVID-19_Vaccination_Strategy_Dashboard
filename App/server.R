559d9098f43cad92474abedd34ec0821c2b62c5a
Andie Payne <andiepayne@outlook.com>
2021-02-11 14:13:08 -0500

    beginning to show plots

--- App/server.R
+++ App/server.R
@@ -2,10 +2,20 @@
 library(shiny)
 library(tidyverse)
 library(cancensus)
+library(plotly)
 
 
 shinyServer(function(input, output) {
     
+    hideTab(inputId = "tabs", target = "1")
+    hideTab("tabs", "2")
+    hideTab("tabs", "3")
+    hideTab("tabs", "4")
+    hideTab("tabs", "5")
+    hideTab("tabs", "6")
+    hideTab("tabs", "7")
+    
+
     #################### Accessing Data and Cleaning
     
     ### Accessing Ontario census data 
@@ -40,102 +50,222 @@ shinyServer(function(input, output) {
         select(Population,  eligiblePopulation, Age16to19, Age20s, Age30s, Age40s, Age50s, Age60s, Age70s, Age80s, Age90s, Age100s)
     
     
-    #### Setting User Inputs
-    startingPop = reactive({input$startingPop})
-    startingCases = reactive({input$originalVirusInfections})
-    time = reactive({input$iter})
     
-    priorityType <- reactive({
-        if(input$strat == "High Population Density"){
-            1
-        }
-        else if(input$strat == "Oldest to Youngest"){
-            2
-        }
-        else if(input$strat == "Youngest to Oldest"){
-            3
-        }
-        else if(input$strat == "Random"){
-            0
-        }
-    })
-    # 
-    observe({print(priorityType())})
-    doses = reactive({as.numeric(input$doses)})
-    vaccPerTime = reactive({input$genericVac})
+    values = reactiveValues()
+    
+    observeEvent(input$submit, {
+       
+        
+        
+        #### Setting User Inputs
+        startingPop = input$startingPop
+        startingCases = input$originalVirusInfections
+        time = input$iter 
+        doses = as.numeric(input$doses)
+        vaccPerTime = input$genericVac
     
     
-    ### Setting constants
-    caseProbability = 279472/populationData$Population #ontario case data as of Monday Feb 8th 2021
-    deathProbability = 6538/279472 #ontario death data as of Monday Feb 8th
+        ### Setting constants
+        caseProbability = 279472/populationData$Population #ontario case data as of Monday Feb 8th 2021
+        deathProbability = 6538/279472 #ontario death data as of Monday Feb 8th
     
-    set.seed(29)
-    populationMatrix = reactive({
-        tibble("PersonID" = NA, "Vaccinated" = NA, "Status" = NA, "PopDensity" = NA, "Age" = NA, "LTC" = NA, "VaccineType" = NA, "Suseptibility" = NA, .rows = startingPop()) %>%
-            mutate(PersonID = c(1:startingPop()), Vaccinated = 0, Status = 0, PopDensity = rbinom(startingPop(), 1, 0.15), Age = round(runif(startingPop(), min = 1, max = 10)), LTC = 0, VaccineType = 0, Suseptibility = 1)})
-    output$popMatrix <- renderTable(populationMatrix())
-    ### Vaccinated - 0 none
-    ###             1 one dose
-    ###             2 one dose
-    ###
-    ### Status -    0 alive and well
-    ###             1 alive but sick and first week infectious
-    ###             2 alive and second week sick
-    ###             3 dead
-    ###
-    ### Pop Density -  1 over high pop dens
-    ###                0 not
-    ###
-    ### Age - first digit of age (1 - teens, 2 - 20s etc)
-    ###
-    ### LTC - indicator for LTC homes - 1 true/0 false
-    ###
-    ### Vaccine Type - 0 unvacc
-    ###               1-4 pfizer (1, just vacc, 2 vacc last week, 3 vacc two weeks ago, 4 third weeks ago and now fully vaccinated)
-    ###               5-9 moderna (5, just vacc, 6 vacc last week, 7 vacc two weeks ago, 8 third weeks ago, 9 four weeks ago and now fully vaccinated)
-    ### Suseptiblilty- 0 not vacc
-    ###               0.48, one dose of pfizer (1 dose has 52% efficacy)
-    ###               0.198, one dose of moderna (1 does has 80.2% efficacy)
-    ###               0.05, two dose pfizer (95% eff)
-    ###               0.059, two dose moderna (95% eff)
+        set.seed(29)
+        populationMatrix = tibble("PersonID" = NA, "Vaccinated" = NA, "Status" = NA, "PopDensity" = NA, "Age" = NA, "LTC" = NA, "VaccineType" = NA, "Suseptibility" = NA, .rows = startingPop) %>%
+            mutate(PersonID = c(1:startingPop), Vaccinated = 0, Status = 0, PopDensity = rbinom(startingPop, 1, 0.15), Age = round(runif(startingPop, min = 1, max = 10)), LTC = 0, VaccineType = 0, Suseptibility = 1)
+
+        ### Vaccinated - 0 none
+        ###             1 one dose
+        ###             2 one dose
+        ###
+        ### Status -    0 alive and well
+        ###             1 alive but sick and first week infectious
+        ###             2 alive and second week sick
+        ###             3 dead
+        ###
+        ### Pop Density -  1 over high pop dens
+        ###                0 not
+        ###
+        ### Age - first digit of age (1 - teens, 2 - 20s etc)
+        ###
+        ### LTC - indicator for LTC homes - 1 true/0 false
+        ###
+        ### Vaccine Type - 0 unvacc
+        ###               1-4 pfizer (1, just vacc, 2 vacc last week, 3 vacc two weeks ago, 4 third weeks ago and now fully vaccinated)
+        ###               5-9 moderna (5, just vacc, 6 vacc last week, 7 vacc two weeks ago, 8 third weeks ago, 9 four weeks ago and now fully vaccinated)
+        ### Suseptiblilty- 0 not vacc
+        ###               0.48, one dose of pfizer (1 dose has 52% efficacy)
+        ###               0.198, one dose of moderna (1 does has 80.2% efficacy)
+        ###               0.05, two dose pfizer (95% eff)
+        ###               0.059, two dose moderna (95% eff)
     
     
-    #### Plotting Tables
-    vaccPop = tibble("newVaccinatedPop" = NA, "cumulativeVaccPop" = NA, "time"= NA) %>% remove_missing(na.rm = TRUE)
-    covidCases = tibble("newCases" = NA, "currentCases" = NA, "cumulativeCases" = NA, "time" = NA)%>% remove_missing(na.rm = TRUE)
-    covidDeaths = tibble("newDeaths" = NA, "cumulativeDeaths" = NA, "time" = NA)%>% remove_missing(na.rm = TRUE)
+        #### Plotting Tables
+        vaccPop = tibble("newVaccinatedPop" = NA, "cumulativeVaccPop" = NA, "time"= NA) %>% remove_missing(na.rm = TRUE)
+        covidCases = tibble("newCases" = NA, "currentCases" = NA, "cumulativeCases" = NA, "time" = NA)%>% remove_missing(na.rm = TRUE)
+        covidDeaths = tibble("newDeaths" = NA, "cumulativeDeaths" = NA, "time" = NA)%>% remove_missing(na.rm = TRUE)
     
 
     
-    ### Establishing vaccination order 
-    vaccinationOrderMatrix <- reactive({
-        if(priorityType() == 0){ #random
-            populationMatrix() %>% filter(Vaccinated == 0) %>% 
-                mutate(VaccOrder = sample(PersonID, startingPop())) %>% 
+        ### Establishing vaccination order 
+        if(input$strat == "Random"){ #random
+            populationMatrix = populationMatrix %>% filter(Vaccinated == 0) %>% 
+                mutate(VaccOrder = sample(PersonID, startingPop)) %>% 
                 arrange(VaccOrder)
-        }
-    
-        else if(priorityType() == 1){ #pop density
-            populationMatrix() %>% filter(Vaccinated == 0) %>% 
+        } else if(input$strat == "High Population Density"){ #pop density
+            populationMatrix = populationMatrix %>% filter(Vaccinated == 0) %>% 
                 arrange(desc(PopDensity)) %>% 
-                mutate(vaccOrder = c(1:startingPop()))
-        }
-    
-        else if(priorityType() == 2){ #oldest first
-            populationMatrix %>% filter(Vaccinated == 0) %>% 
+                mutate(vaccOrder = c(1:startingPop))
+        } else if(input$strat == "Oldest to Youngest"){ #oldest first
+            populationMatrix = populationMatrix %>% filter(Vaccinated == 0) %>% 
                 arrange(desc(Age)) %>% 
-                mutate(vaccOrder = c(1:startingPop()))
+                mutate(vaccOrder = c(1:startingPop))
+        } else if(input$strat == "Youngest to Oldest"){ #youngest first
+            populationMatrix = populationMatrix %>% filter(Vaccinated == 0) %>% 
+                arrange(Age) %>% 
+                mutate(vaccOrder = c(1:startingPop))
         }
     
-        else if(priorityType() == 3){ #youngest first
-            populationMatrix %>% filter(Vaccinated == 0) %>% 
-                arrange(Age) %>% 
-                mutate(vaccOrder = c(1:startingPop()))
-        }
+    
+    
+
+    ### Simulation Time
+     for (currentTime in 1:time){
+         
+         if (currentTime == 1){
+             susceptiblePopulation = reactive({populationMatrix %>% filter(Status == 0)})
+             newInfected = reactive({sample(susceptiblePopulation()$PersonID, startingCases)})
+             populationMatrix = populationMatrix %>% mutate(Status = ifelse(test = PersonID %in% sample(susceptiblePopulation()$PersonID, startingCases), yes = 1, no = Status))
+             
+             
+         }
+         else{
+             ### Infection Round
+             currentCasesMatrix = populationMatrix %>% filter(Status == 1 | Status == 2)
+             currentCases = as.numeric(count(currentCasesMatrix))
+             
+             ## deaths
+             deathsThisWeek = rbinom(1, currentCases, deathProbability)
+             newDeaths = sample(currentCasesMatrix$PersonID, deathsThisWeek)
+             populationMatrix = populationMatrix %>% mutate(Status = ifelse(test = PersonID %in% newDeaths, yes = 3, no = Status))
+             
+             ## people get better
+             populationMatrix = populationMatrix %>% mutate(Status = ifelse(test = Status == 2, yes = 0, no = Status),
+                                                            Status = ifelse(test = Status == 1, yes = 2, no = Status))
+             
+             ## infected
+             infectedThisWeek = rpois(1, lambda = 1.05*currentCases*(1/2)) #1.05 is ontario's median Re value
+             currentHealthy = as.numeric(count(populationMatrix %>% filter(Status == 0)))
+             if (infectedThisWeek > currentHealthy){
+                 infectedThisWeek = currentHealthy
+             }
+             
+             
+             susceptiblePopulation = populationMatrix %>% filter(Status == 0)
+             contactIDs= sample(susceptiblePopulation$PersonID, infectedThisWeek)
+             contactPopulation = populationMatrix %>% filter(PersonID %in% contactIDs) %>% 
+                 mutate(Infected = rbinom(contactIDs, 1, Suseptibility))
+             infectedIDs = contactPopulation %>% filter(Infected == 1)
+             
+             populationMatrix = populationMatrix %>% mutate(Status = ifelse(test = PersonID %in% infectedIDs$PersonID, yes = 1, no = Status))
+             
+         }
+         
+         ### Vaccination Round - assumption 2nd dose does not impact weekly numbers
+         # currently only built on Pfizer
+         if(vaccPerTime !=0){
+            toVaccinate = populationMatrix %>% filter(Vaccinated == 0 & Status == 0) %>% slice(1:(vaccPerTime))
+             populationMatrix = populationMatrix %>% 
+                mutate(Vaccinated = ifelse(test = PersonID %in% toVaccinate$PersonID, yes = 1, no = Vaccinated),
+                        Suseptibility = ifelse(test = PersonID %in% toVaccinate$PersonID, yes = 0.48, no = Suseptibility), 
+                        VaccineType = ifelse(test = PersonID %in% toVaccinate$PersonID, yes = 1, no = VaccineType))
+         
+         
+         
+            ##second round vaccinations
+            if(doses == 2){
+                populationMatrix = populationMatrix %>% mutate(VaccineType = ifelse(test = VaccineType > 0 & VaccineType < 4, yes = VaccineType+1, no = VaccineType), #pfizer boost
+                                                                Vaccinated = ifelse(test = VaccineType == 4, yes = 2, no = Vaccinated),
+                                                                Suseptibility = ifelse(test = VaccineType == 4, yes = 0.05, no = Suseptibility),
+                                                                VaccineType = ifelse(test = PersonID %in% toVaccinate, yes = 1, no = VaccineType), #those just vaccinated
+                                                                
+                                                                VaccineType = ifelse(test =  VaccineType > 4 & VaccineType < 9, yes = VaccineType+1, no = VaccineType), #moderna Boost
+                                                                Vaccinated = ifelse(test = VaccineType == 9, yes = 2, no = Vaccinated),
+                                                                Suseptibility = ifelse(test = VaccineType == 9, yes = 0.059, no = Suseptibility))
+             
+            }
+         }
+         
+         
+         cumulativeVaccinations = as.numeric(count(populationMatrix %>% filter(VaccineType!=0)))
+         vaccPop = vaccPop %>% add_row("newVaccinatedPop" = vaccPerTime, "cumulativeVaccPop" = cumulativeVaccinations, "time" = currentTime)
+         
+         newCasesINT = as.numeric(count(populationMatrix %>% filter(Status == 1)))
+         activeCases = as.numeric(count(populationMatrix %>% filter(Status == 1 | Status == 2)))
+         if(currentTime == 1){
+             cumulativeCases = newCasesINT
+         }  else{
+             cumulativeCases = as.numeric(covidCases$cumulativeCases[currentTime-1])+newCasesINT
+         }
+         covidCases = covidCases %>% add_row("newCases" = newCasesINT, "currentCases" = activeCases, "cumulativeCases" = cumulativeCases, "time" = currentTime)
+         
+         
+         if(currentTime == 1){
+             newDeathsINT = 0
+         }  else{
+             newDeathsINT= length(newDeaths)
+         }
+         deaths = populationMatrix %>% filter(Status == 3)
+         cumulativeDeathsINT = as.numeric(count(deaths))
+         covidDeaths = covidDeaths %>% add_row("newDeaths" = newDeathsINT, "cumulativeDeaths" = cumulativeDeathsINT, "time" = currentTime)
+         
+          
+         
+     }
+     
+    
+    values$populationMatrix = populationMatrix
+    values$covidCases = covidCases
+    values$covidDeaths = covidDeaths
+    values$vaccPop = vaccPop
+    showTab("tabs", "1")
+    showTab("tabs", "2")
+    showTab("tabs", "3")
+    showTab("tabs", "4")
+    showTab("tabs", "5")
+    showTab("tabs", "6")
+    showTab("tabs", "7")
     })
     
     
-    output$vaccMatrix <- renderTable(vaccinationOrderMatrix())
+    output$vaccMatrix <- renderTable(values$covidCases)
+    
+    output$newCasesPlot <- renderPlot({
+        values$covidCases %>%  ggplot(aes(x=time, y=newCases)) +
+            geom_line(col = "blue")+
+            geom_point(col = "blue")+
+            xlab("Week")+
+            ylab("New Cases")+
+            ggtitle("New Cases Per Week")
+    })
+    
+    output$cumulativeCasesPlot <- renderPlotly({
+        plot = values$covidCases %>%  ggplot(aes(x=time, y=cumulativeCases)) +
+            geom_line(col = "blue")+
+            geom_point(col = "blue")+
+            xlab("Week")+
+            ylab("Cumulative Cases")+
+            ggtitle("Cumulative Cases Per Week")
+        ggplotly(plot) %>% config(displayModeBar = FALSE)
+    })
+    
+    output$activeCasesPlot <- renderPlot({
+        values$covidCases %>%  ggplot(aes(x=time, y=currentCases)) +
+            geom_line(col = "blue")+
+            geom_point(col = "blue")+
+            xlab("Week")+
+            ylab("Active Cases")+
+            ggtitle("Active Cases Per Week")
+    })
     
     
     
--- App/ui.R
+++ App/ui.R
@@ -23,7 +23,7 @@ shinyUI(fluidPage(
             
             h3("Initial Populations"),
             numericInput("startingPop", "Total Population", 10000),
-            numericInput("originalVirusInfections", "SARS-CoV-2", 0),
+            numericInput("originalVirusInfections", "SARS-CoV-2", 500),
             #unsure if i want to keep the variations
             #numericInput("UKVirusInfections", "SARS-CoV-2 variant B.1.1.7 (UK Strain)", 0),
             #numericInput("southAfricaVirusInfections", "SARS-CoV-2 variant B.1.351 (South African Strain)", 0),
@@ -42,19 +42,24 @@ shinyUI(fluidPage(
             #numericInput("alreadyVac", "Number of People Already Vaccinated", 0),
             
             h3("Simulation Length"),
-            numericInput("iter", "Iterations", 5)
+            numericInput("iter", "Iterations", 5),
+            
+            actionButton("submit", "Start Simulation")
             
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-            tabsetPanel(tabPanel("Projected New Cases", htmlOutput("casesText")),
-                        tabPanel("Projected Deaths", textOutput("deathText")),
-                        tabPanel("Vaccinated Individuals", textOutput("vaccText")),
-                        tabPanel("Visualization", textOutput("mapsText")),
-                        tabPanel("Data Manipulation and Limitations", htmlOutput("dataLimText")),
-                        tabPanel("Projected New Cases", tableOutput("vaccMatrix")),
-                        tabPanel("Author", textOutput("authorText")))
+            
+                tabsetPanel(id = "tabs",tabPanel("Projected Cases", verticalLayout(plotOutput("activeCasesPlot"), plotOutput("newCasesPlot"), plotlyOutput("cumulativeCasesPlot")), value = 1),
+                        tabPanel("Projected Deaths", textOutput("deathText"), value = 2),
+                        tabPanel("Vaccinated Individuals", textOutput("vaccText"), value = 3),
+                        tabPanel("Visualization", textOutput("mapsText"), value = 4),
+                        tabPanel("Data Manipulation and Limitations", htmlOutput("dataLimText"), value = 5),
+                        tabPanel("Projected New Cases 2", tableOutput("vaccMatrix"), value = 6),
+                        tabPanel("Author", textOutput("authorText"), value = 7)
+                )
+            
         )
     )
 ))
