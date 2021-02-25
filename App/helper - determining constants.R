8801df8ea401d0fa96bded3ee2e10d3c1554b5c0
Andie Payne <andiepayne@outlook.com>
2021-02-25 13:51:28 -0500

    added Re calculations

--- App/helper - determining constants.R
+++ App/helper - determining constants.R
@@ -12,3 +12,15 @@ ontariansLivingInHighPopDensity = torontoPop+mississaugaPop
 ## scaling COVID constants to a 100,000 person population
 pfizerVaccinations = (512655/11078186)*100000  ## number of pfizer vaccines distributed on Feb 18th divided by Ontario's eligible population (found in server.R)
 modernaVaccinations = (170600/11078186)*100000 ## number of Moderna vaccines distributed on Feb 18th div by Ontario's eligible population
+
+
+## Re values for population density
+re <- read_csv("App/Data/re_estimates_on.csv")$`Re` %>% sort()  ## pulled Feb 25
+thirdsOfRe = round(length(re)/3)
+
+medianRe = median(re)
+
+lowestRe = median(re[(1:thirdsOfRe)])
+middleRe = median(re[((thirdsOfRe+1):(2*thirdsOfRe))])
+highestRe = median(re[((2*thirdsOfRe+1):length(re))])
+
